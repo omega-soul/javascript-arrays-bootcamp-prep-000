cd37d8da3331a1482c7f08b4f150e14cf576b69c
vincent <omegaortiz1997@gmail.com>
2018-02-17 19:12:30 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,6 +5,6 @@ function addElementToBeginningOfArray(array, element) {
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  return  new Array("foo", ...array);
+  return  new Array("foo", 1);
 }
 
