3eddc0a5d712281136d5e5dd540cf42c3d610728
vincent <omegaortiz1997@gmail.com>
2018-02-17 19:05:16 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,10 @@
+var chocolateBars = ["snickers", "hundred", "grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(array, element) {
+  return ["foo", 1];
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  return  array.unshift("foo", 1);
+}
+
