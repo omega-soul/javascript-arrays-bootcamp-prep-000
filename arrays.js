c9ab68abe393513cf1254b8e94c58aa2d9aac617
vincent <omegaortiz1997@gmail.com>
2018-02-18 20:17:04 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -32,3 +32,28 @@ function removeElementFromBeginningOfArray(array) {
   return array;
 }
 
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop();
+  return array;
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+}
