bcedc7af9cd6ec0a51503852bf147f00b7bc73c5
vincent <omegaortiz1997@gmail.com>
2018-02-18 19:41:30 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -19,5 +19,5 @@ function destructivelyAddElementToEndOfArray(array, element) {
 }
 
 function accessElementInArray(array, index) {
-  return (accessElementInArray[1]);
+  return (accessElementInArray[3]);
 }
